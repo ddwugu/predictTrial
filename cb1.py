b859d4b3995ad02afdb646b8ea48bf4e18f55d28
ddwugu <136143092+ddwugu@users.noreply.github.com>
2024-03-14 08:37:47 +0700

    cb1.py

--- cb1.py
+++ cb1.py
@@ -1,48 +1,3 @@
-import pickle
-import streamlit as st
-
-# Load the model
-try:
-    with open('Pred_lokasi11.sav', 'rb') as file:
-        LokasiKM = pickle.load(file)
-except Exception as e:
-    st.error(f"Error loading the model: {e}")
-    LokasiKM = None  # Assign None if there is an error loading the model
-
-# Web Title
-st.title('Pertamina Field Jambi')
-st.subheader('Prediksi Lokasi Kebocoran Line BJG-TPN')
-
-# User Inputs
-import requests
-import pandas as pd
-
-# Masukkan API key dan Channel ID
-READ_API_KEY = 'SPYMD6ONS3YT6HKN'
-CHANNEL_ID = '2405457'
-FIELD_ID_1 = '1'
-FIELD_ID_2 = '3'
-
-# URL untuk mengakses data dari ThingSpeak untuk field 1
-url_field_1 = f'https://api.thingspeak.com/channels/{CHANNEL_ID}/fields/{FIELD_ID_1}.csv?api_key={READ_API_KEY}'
-
-# URL untuk mengakses data dari ThingSpeak untuk field 2
-url_field_2 = f'https://api.thingspeak.com/channels/{CHANNEL_ID}/fields/{FIELD_ID_2}.csv?api_key={READ_API_KEY}'
-
-# Function to fetch data
-def fetch_data():
-    response_field_1 = requests.get(url_field_1)
-    response_field_2 = requests.get(url_field_2)
-    if response_field_1.status_code == 200 and response_field_2.status_code == 200:
-        data_field_1 = pd.read_csv(url_field_1)
-        data_field_2 = pd.read_csv(url_field_2)
-        Titik_1_PSI = data_field_1['field1'].iloc[0] if not data_field_1.empty else None
-        Titik_2_PSI = data_field_2['field3'].iloc[0] if not data_field_2.empty else None
-        return Titik_1_PSI, Titik_2_PSI
-    else:
-        return None, None
-
-# Function to predict location
 def predict_location(Titik_1_PSI, Titik_2_PSI):
     if Titik_1_PSI is not None and Titik_2_PSI is not None:
         try:
@@ -50,28 +5,20 @@ def predict_location(Titik_1_PSI, Titik_2_PSI):
             b = 86 - float(Titik_2_PSI)
             prediksi_lokasi = LokasiKM.predict([[a, b]])
             if prediksi_lokasi[0] == 0: # titik nol
+                Nilai Titik_1_PSI: {Titik_1_PSI}
+                Nilai Titik_1_PSI: {Titik_2_PSI}
                 suspect_loct = 'It is safe that there is no fluid flowing'
+                
             elif prediksi_lokasi[0] >= 26.3: # total panjang trunkline
+                Nilai Titik_1_PSI: {Titik_1_PSI}
+                Nilai Titik_1_PSI: {Titik_2_PSI}
                 suspect_loct = 'Safe, there are no'
             else:
+                Nilai Titik_1_PSI: {Titik_1_PSI}
+                Nilai Titik_1_PSI: {Titik_2_PSI}
                 suspect_loct = f'!!!estimated  location {prediksi_lokasi[0]} KM'
             return suspect_loct
         except Exception as e:
             return f"Error predicting location: {e}"
     else:
         return "Nilai 'Titik_1_PSI' atau 'Titik_2_PSI' tidak tersedia, menggunakan nilai sebelumnya jika ada."
-
-# Placeholder for real-time updates
-placeholder = st.empty()
-
-# Continuously update the predictions
-while True:
-    Titik_1_PSI, Titik_2_PSI = fetch_data()
-    if Titik_1_PSI is not None and Titik_2_PSI is not None:
-        placeholder.empty()  # Clear hasil sebelumnya
-        placeholder.write(f'Nilai Titik_1_PSI: {Titik_1_PSI}')
-        placeholder.write(f'Nilai Titik_2_PSI: {Titik_2_PSI}')
-        location_prediction = predict_location(Titik_1_PSI, Titik_2_PSI)
-        placeholder.write(location_prediction)
-    else:
-        st.warning("Nilai 'Titik_1_PSI' atau 'Titik_2_PSI' tidak tersedia, menggunakan nilai sebelumnya jika ada.")
